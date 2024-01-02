74e0ccebf40812db20c69749ad206a5a52cec4a1
mooooooi <emwings@outlook.com>
2024-01-02 18:27:31 +0800

    temp

---
+++ Mobx.Core/M.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Mobx.Core
+{
+    public static class M
+    {
+        private static uint m_GlobalBatchDepth = 0;
+        private static uint m_IdGenerator = 0;
+
+        public ref struct BatchDepthScope
+        {
+            private bool m_Created;
+
+            public uint BatchDepth => m_GlobalBatchDepth;
+
+            public static BatchDepthScope Create()
+            {
+                m_GlobalBatchDepth++;
+                return new BatchDepthScope() { m_Created = true };
+            }
+
+            public void Dispose()
+            {
+                if (!m_Created) return;
+                m_GlobalBatchDepth--;
+                m_Created = false;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void StartBatch()
+        {
+            m_GlobalBatchDepth++;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void EndBatch()
+        {
+            m_GlobalBatchDepth--;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SetValue<T>(ref T field, in T value)
+        {
+            field = value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref T GetValue<T>(ref T field)
+        {
+            StartBatch();
+            EndBatch();
+            return ref field;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsInBatching() => m_GlobalBatchDepth != 0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ReportObervered(ref uint m_Id)
+        {
+            if (!IsInBatching()) return;
+            if (m_Id == 0) m_Id = ++m_IdGenerator;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ReportChanged(ref uint m_Id)
+        {
+            if (!IsInBatching()) return;
+            if (m_Id == 0) m_Id = ++m_IdGenerator;
+        }
+    }
+
+    public class ObservableValue<T>
+    {
+        private uint m_Id;
+        [AllowNull]
+        private T m_Field;
+
+        public ObservableValue()
+        {
+            m_Id = 0;
+            m_Field = default;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetValue(in T value)
+        {
+            M.ReportChanged(ref m_Id);
+            m_Field = value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T GetValue()
+        {
+            M.ReportObervered(ref m_Id);
+            return m_Field;
+        }
+    }
+
+    public struct TestData
+    {
+        private ObservableValue<int> __a;
+        public int a { get => __a.GetValue(); set => __a.SetValue(value); }
+    }
+}
---
+++ src/Mobx.Core/Newsomethings.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Mobx.Core
+{
+    public interface IObserver
+    {
+        int Length { get; }
+        bool this[int index] { get; set; }
+        void Clear();
+    }
+
+    public enum ObserverMode
+    {
+        Set = 1 << 0, Get = 1 << 1, GetSet = Set | Get
+    }
+
+    public unsafe struct Observer64
+    {
+        const int k_MaskLength = 2;
+        const int k_MaskPerLength = sizeof(int);
+        fixed int mask[k_MaskLength];
+        public readonly int Length => k_MaskLength * k_MaskPerLength;
+        public ObserverMode Mode;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Mark(int index)
+        {
+            mask[index / k_MaskPerLength] |= 1 << (index % k_MaskPerLength);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Set<T>(int index, ref T field, in T value)
+        {
+            if ((Mode & ObserverMode.Set) > 0) Mark(index);
+            field = value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ref T Get<T>(int index, ref T field)
+        {
+            if ((Mode & ObserverMode.Get) > 0) Mark(index);
+            return ref field;
+        }
+
+        public void Clear()
+        {
+            mask[0] = 0;
+            mask[1] = 0;
+        }
+    }
+
+    public unsafe partial interface IData
+    {
+        int a { get; set; }
+        string b { get; set; }
+    }
+
+    public partial interface IData
+    {
+        public struct Implement : IData
+        {
+            private Observer64 __o__;
+
+            private int a__k__BackingField;
+            public int a
+            {
+                get => __o__.Get(0, ref a__k__BackingField);
+                set => __o__.Set(0, ref a__k__BackingField, value);
+            }
+
+            private string b__k__BackingField;
+            public string b
+            {
+                get => __o__.Get(1, ref b__k__BackingField);
+                set => __o__.Set(1, ref b__k__BackingField, value);
+            }
+        }
+
+        public static Implement Create() => new Implement();
+    }
+}
